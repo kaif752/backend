a4687dff18dc75bd9863e16a124dab22929a6542
M Kaif Shekh <164408871+kaif752@users.noreply.github.com>
2025-07-21 22:55:48 +0530

    Update server.js
    basic

--- server.js
+++ server.js
@@ -2,24 +2,22 @@ require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const db = require('./config/db'); // Just for testing connection
+const db = require('./config/db'); 
 const contactRoutes = require('./routes/contactRoutes');
 
 const app = express();
 
-// Middleware
 app.use(cors());
 app.use(bodyParser.json());
 
-// Test DB connection (optional)
 db.query('SELECT 1 + 1 AS solution')
-  .then(() => console.log('✅ Connected to MySQL'))
-  .catch(err => console.error('❌ MySQL connection error:', err));
+  .then(() => console.log('Connected to MySQL'))
+  .catch(err => console.error('MySQL connection error:', err));
 
-// Routes
+
 app.use('/api/contact', contactRoutes);
 
-// Error Handling Middleware
+
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).json({ error: 'Something went wrong!' });
@@ -27,5 +25,5 @@ app.use((err, req, res, next) => {
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`🚀 Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
